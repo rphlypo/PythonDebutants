430707ac282051b6cc543bddd50585056a381860
Ronald Phlypo <ronald.phlypo@gipsa-lab.grenoble-inp.fr>
2017-11-06 11:15:54 +0100

    adapted path to grunt

--- launchme.sh
+++ launchme.sh
@@ -3,6 +3,6 @@
 echo Starting the jupyter server on port 8842
 gnome-terminal -x jupyter notebook --port 8842 ./notebook &&
 echo Starting the presentation
-grunt --gruntfile ./revealjs/Gruntfile.js serve
+grunt --gruntfile ./reveal.js/Gruntfile.js serve
 
 exit 0
