105adf110fbbbdb177afb2e941d76c1c1b085fbf
Ronald Phlypo Amate <ronald.phlypo@grenoble-inp.fr>
2017-11-11 09:56:03 +0100

    now opens two gnome-consoles and call 'npm start' rather than 'grunt serve'

--- launchme.sh
+++ launchme.sh
@@ -3,6 +3,6 @@
 echo Starting the jupyter server on port 8842
 gnome-terminal -x jupyter notebook --port 8842 ./notebook &&
 echo Starting the presentation
-grunt --gruntfile ./revealjs/Gruntfile.js serve
+gnome-terminal -x grunt --gruntfile ./revealjs/Gruntfile.js serve
 
 exit 0
