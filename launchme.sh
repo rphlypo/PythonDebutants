78c93c6db573fe3033db18642e01955fed29c4d5
Ronald Phlypo Amate <ronald.phlypo@grenoble-inp.fr>
2017-11-25 23:14:47 +0100

    npm start rather than grunt serve, adding feedback

--- launchme.sh
+++ launchme.sh
@@ -2,7 +2,7 @@
 
 echo Starting the jupyter server on port 8842
 gnome-terminal -x jupyter notebook --port 8842 ./notebook &&
-echo Starting the presentation
-gnome-terminal -x grunt --gruntfile ./revealjs/Gruntfile.js serve
-
+echo Starting the presentation on port 8000
+gnome-terminal -x npm start --prefix ./reveal.js &&
+echo Be patient ... serving data to your web-browser
 exit 0
